c9b5c92ae5384fa861035aa674149798d8e1a5c2
Kohut Artem <sirin@MacBook-Pro-Kohut.local>
2025-06-23 10:03:07 +0300

    six

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/palyanitsa', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
     console.log('✅ Підключено до MongoDB');
   } catch (err) {
